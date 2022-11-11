68b040694cabfe2feaad4f48c172820d8c65f8cd
DeepankRx <deepankpushpad10@gmail.com>
2022-11-12 00:40:41 +0530

    all products page

--- src/components/ProductCategories.jsx
+++ src/components/ProductCategories.jsx
@@ -1,50 +1,100 @@
-import React from 'react'
-
+import React, { useState, useEffect, useContext } from 'react';
+import { getProducts } from '../services/api-services';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import AuthContext from '../store/AuthContext';
 const ProductCategories = () => {
+  const authCtx=useContext(AuthContext);
+  const userId = authCtx.userid._id;
+  console.log(userId);
+  const [buttonText, setButtonText] = useState('Place Bid');
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    getProducts()
+      .then((res) => {
+        setProducts(res.data.products);
+        console.log(res.data);
+      })
 
-    const Box=()=>{
-        return(
-            <div className='p-4 smrev:p-0 flex justify-center items-center flex-col'>
-            <div className='smrev:w-[100%] w-[90%] bg-gray-300 h-60 rounded-lg overflow-hidden' ></div>
+      .catch((err) => console.log(err.response.data));
+  }, []);
 
-
-            <div className='smrev:w-[100%] w-[90%] flex  justify-between p-2'>
-              <div className='flex items-center  space-x-2'>
-              <div className='w-20 h-20 bg-gray-100 rounded-full'>
-              </div>
-              <div>
-                <p className='font-bold'>John Dales</p>
-                <p className='text-xs'>Sponsored by </p>
-                </div>
-              </div>
-              <div className='text-xl flex justify-center items-center'> 5999</div>
-            </div>
-
+  const Box = ({
+    sellerName,
+    price,
+    sellerImage,
+    productImage,
+    city,
+    state,
+    sellerId
+  }) => {
+    return (
+      <div className="p-4 smrev:p-0 flex justify-center items-center flex-col">
+        <div className="smrev:w-[100%] w-[90%] bg-gray-300 h-60 rounded-lg overflow-hidden">
+          <img src={productImage} alt="product" className="w-full h-full" />
         </div>
-        )
-    }
-  return (
-    <div className='p-8 smrev:p-4 '>  
-    <div className='grid lg:grid-cols-3 gap-4  sm:grid-cols-2 smrev:grid-cols-1 smrev:space-y-4'>
-        <Box/>
-        <Box/>
-        <Box/>
-        <Box/>
-        <Box/>
-        <Box/>
-        <Box/>
-        <Box/>
-        <Box/>
-        <Box/>
-        <Box/>
-        <Box/>
-        <Box/>
-        <Box/>
-        <Box/>
-        <Box/>
-    </div>
-    </div>
-  )
-}
 
-export default ProductCategories
+        <div className="smrev:w-[100%] w-[90%] flex  justify-between p-2">
+          <div className="flex items-center  space-x-2">
+            <div className="w-20 h-20 bg-gray-100 rounded-full">
+              <img
+                src={sellerImage}
+                alt="seller"
+                className="w-full h-full rounded-full"
+              />
+            </div>
+            <div>
+              <p className="font-bold">{sellerName}</p>
+              <p className="text-xs uppercase">
+                <FontAwesomeIcon
+                  className="text-lg text-tertiary"
+                  icon={faLocationDot}
+                  color="red"
+                ></FontAwesomeIcon>
+                {'  '}
+                {city},{state}
+              </p>
+            </div>
+          </div>
+          <div className="text-xl flex justify-center items-center">
+            ₹{price}
+          </div>
+        </div>
+
+        <button
+          className={`bg-tertiary ${!userId===sellerId ? "bg-secondary":"bg-gray-500"} p-2 rounded-lg w-[90%] smrev:w-[100%]
+
+            ${
+              userId===sellerId
+              ? "text-white":"text-black"
+            }
+           font-semibold
+        `}
+        >
+          {
+            userId === sellerId ? "Your Product" : "Place Bid"
+          }
+        </button>
+      </div>
+    );
+  };
+  return (
+    <div className="p-8 smrev:p-4 ">
+      <div className="grid lg:grid-cols-3 gap-4  sm:grid-cols-2 smrev:grid-cols-1 smrev:space-y-4">
+        {products.map((product) => (
+          <Box
+            sellerName={product.seller.name}
+            price={product.basePrice}
+            sellerImage={product.seller.profilePic}
+            productImage={product.image}
+            city={product.seller.city}
+            state={product.seller.state}
+            sellerId = {product.seller._id}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductCategories;
